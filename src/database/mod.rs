602c84779ead1a007c143d6f8393b83cb90839c1
Zarif Atai <z.atai@protonmail.com>
2025-04-24 17:24:17 +0200

    Removed unnecessary borrow

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -3,7 +3,7 @@ pub mod operations;
 use rusqlite::{Connection, Result};
 use std::path::PathBuf;
 
-pub fn establish_connection(database_path: &PathBuf) -> Result<Connection, rusqlite::Error> {
+pub fn establish_connection(database_path: PathBuf) -> Result<Connection, rusqlite::Error> {
     Connection::open(database_path)
 }
 
