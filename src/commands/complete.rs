e3c7eff69295980f03673ab0db56a8b42155bce8
Zarif Atai <z.atai@protonmail.com>
2025-04-25 18:30:31 +0200

    Improved naming and removed commented code

--- src/commands/complete.rs
+++ src/commands/complete.rs
@@ -7,13 +7,11 @@ use crate::utils;
 pub fn run(connection: Connection, name: Option<String>, id: Option<i32>) -> Result<()> {
     match utils::resolve_identifier(name, id) {
         Some(Identifier::Id(item_id)) => {
-            database::operations::update_item_by_id(connection, item_id)?;
-            // connection.execute("UPDATE item SET active = 0 WHERE id = ?1;", (item_id,))?;
+            database::operations::complete_item_by_id(connection, item_id)?;
             println!("Item completed!");
         }
         Some(Identifier::Name(item_name)) => {
-            database::operations::update_item_by_name(connection, item_name)?;
-            // connection.execute("UPDATE item SET active = 0 WHERE name = ?1;", (&item_name,))?;
+            database::operations::complete_item_by_name(connection, item_name)?;
             println!("Item completed!");
         }
         None => {
