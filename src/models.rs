9c9d36b26e2bc27b4dddf2667fd96d172b113679
Zarif Atai <z.atai@protonmail.com>
2025-04-25 18:31:35 +0200

    Added sqlite models for migration

--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug, Clone)]
 pub struct Item {
     pub name: String,
@@ -9,4 +11,31 @@ pub struct Item {
 pub enum Identifier {
     Id(i32),
     Name(String),
+}
+
+pub struct SqliteColumn {
+    pub name: String,
+    pub ty: SqliteColumnType,
+}
+
+#[derive(Clone, Copy)]
+#[allow(dead_code)]
+pub enum SqliteColumnType {
+    Integer,
+    Text,
+    Blob,
+    Real,
+    Numeric,
+}
+
+impl fmt::Display for SqliteColumnType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            SqliteColumnType::Integer => write!(f, "INTEGER"),
+            SqliteColumnType::Text => write!(f, "TEXT"),
+            SqliteColumnType::Blob => write!(f, "BLOB"),
+            SqliteColumnType::Real => write!(f, "REAL"),
+            SqliteColumnType::Numeric => write!(f, "NUMERIC"),
+        }
+    }
 }
