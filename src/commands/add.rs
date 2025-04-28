02052690998b63630e469fc77c569ac004526e7f
Zarif Atai <z.atai@protonmail.com>
2025-04-29 00:08:41 +0200

    Formatting

--- src/commands/add.rs
+++ src/commands/add.rs
@@ -12,7 +12,6 @@ pub fn run(
 ) -> Result<()> {
     let create_date = Local::now().naive_local();
     let due_date = utils::parse_datetime_str(due_date);
-
     database::operations::add_item(connection, name, description, create_date, due_date)?;
     println!("Item added!");
     Ok(())
